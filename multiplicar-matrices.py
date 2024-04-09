bf653f74738d36a208a330f858270c5a3a892187
FlorenciaRossi <flrossi@estudiantes.unsam.edu.ar>
2024-04-09 11:10:12 -0300

    multiplicar matrices

--- multiplicar-matrices.py
+++ multiplicar-matrices.py
@@ -2,6 +2,7 @@
 matrizA = [[1, 4],[2, 5],[3, 6]]
 matrizB = [[5, 1, -2],[-1, 0 , 3]]
 mC = [[1,1,0], [2,1,1], [3,1,-1]]
+
 def getMatriz(m):
     # i -> columna
     # j -> fila
@@ -22,7 +23,7 @@ def multiplicar(m, n):
     if ( numColumnas(m) != numFilas(n)):
         raise ValueError("No se pueden multiplicar las matrices")
     mNueva = [[0] * numFilas(m) for _ in range(numColumnas(n))]
-    #mNueva = [ [0] * numFilas(m) ] * numColumnas(n)
+
     #lleno por filas
     for i in range(len(m[0])):   
         #k columna de n
